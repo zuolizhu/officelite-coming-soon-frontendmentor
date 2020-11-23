03149c68bcee63b85e8d02a9fe9803af1b07b33c
zuolizhu <zuolizhu5960x@gmail.com>
2020-11-22 20:46:44 -0500

    home page hero finished

---
+++ components/home/HomeHero.js
@@ -0,0 +1,20 @@
+import Link from 'next/link'
+
+export default function HomeHero() {
+  return (
+    <section className="home-hero">
+      <div className="container container--home">
+        <div className="home-hero__content">
+          <h1 className="t-h1">A simple solution to complex tasks is coming soon</h1>
+          <p className="t-body1 t-light-gray home-hero__content__body">Say goodbye to inefficient juggling of multiple apps, teams, and projects. Officelite is the new collaboration platform built with an intuitive interface to improve productivity.</p>
+          <Link href="/signup">
+            <a className="btn btn--blue btn-shadow-blue">Get Started</a>
+          </Link>
+        </div>
+        <div className="home-hero__image">
+          <img src="/assets/home/illustration-charts.svg" alt="home hero" />
+        </div>
+      </div>
+    </section>
+  )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,10 @@
 import Layout from '../components/Layout'
+import HomeHero from '../components/home/HomeHero'
 
 export default function Home() {
   return (
     <Layout>
-      
+      <HomeHero />      
     </Layout>
   )
 }
